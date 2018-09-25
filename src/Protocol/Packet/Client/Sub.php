3253bc8bf563d2f4b605ac01277188842e5c0384
Markus Reinhold <markusreinhold@icloud.com>
2018-09-25 22:51:17 +0200

    Add the sub protocol packet

---
+++ src/Protocol/Packet/Client/Sub.php
@@ -0,0 +1,67 @@
+<?php
+declare(strict_types=1);
+
+namespace Marein\Nats\Protocol\Packet\Client;
+
+use Marein\Nats\Protocol\Model\Subject;
+use Marein\Nats\Protocol\Model\SubscriptionId;
+
+final class Sub implements Packet
+{
+    /**
+     * @var Subject
+     */
+    private $subject;
+
+    /**
+     * @var SubscriptionId
+     */
+    private $subscriptionId;
+
+    /**
+     * @var string|null
+     */
+    private $queueGroup;
+
+    /**
+     * Sub constructor.
+     *
+     * @param Subject        $subject
+     * @param SubscriptionId $subscriptionId
+     */
+    public function __construct(Subject $subject, SubscriptionId $subscriptionId)
+    {
+        $this->subject = $subject;
+        $this->subscriptionId = $subscriptionId;
+    }
+
+    /**
+     * Add the queue group field for this packet.
+     *
+     * @param string $queueGroup
+     *
+     * @return Sub
+     */
+    public function withQueueGroup(string $queueGroup): Sub
+    {
+        $this->queueGroup = $queueGroup;
+
+        return $this;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function pack(): string
+    {
+        $message = 'SUB ' . (string)$this->subject;
+
+        if ($this->queueGroup !== null) {
+            $message .= ' ' . $this->queueGroup;
+        }
+
+        $message .= ' ' . (string)$this->subscriptionId . Packet::MESSAGE_TERMINATOR;
+
+        return $message;
+    }
+}
---
+++ tests/unit/Protocol/Packet/SubTest.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace Marein\Nats\Protocol\Packet\Client;
+
+use Marein\Nats\Protocol\Model\Subject;
+use Marein\Nats\Protocol\Model\SubscriptionId;
+use PHPUnit\Framework\TestCase;
+
+class SubTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider packetProvider
+     *
+     */
+    public function itShouldBePacked(Sub $sub, string $packed): void
+    {
+        $this->assertSame($packed, $sub->pack());
+    }
+
+    /**
+     * @return array
+     */
+    public function packetProvider(): array
+    {
+        $subscriptionId = SubscriptionId::random();
+
+        return [
+            [
+                (new Sub(new Subject('subject'), $subscriptionId)),
+                'SUB subject ' . (string)$subscriptionId . "\r\n"
+            ],
+            [
+                (new Sub(new Subject('subject'), $subscriptionId))->withQueueGroup('queueGroup'),
+                'SUB subject queueGroup ' . (string)$subscriptionId . "\r\n"
+            ]
+        ];
+    }
+}
