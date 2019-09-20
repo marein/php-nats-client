ae84e703034413280cbf0453ac4c34f75f548af4
Markus Reinhold <markusreinhold@icloud.com>
2019-09-20 20:52:00 +0200

    Add test for socket

---
+++ tests/unit/Connection/SocketTest.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace Marein\Nats\Protocol\Packet\Client;
+
+use Marein\Nats\Connection\Socket;
+use PHPUnit\Framework\TestCase;
+
+class SocketTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function itCanBeCreated(): void
+    {
+        $expectedHost = '127.0.0.1';
+        $expectedPort = 4222;
+
+        $socket = new Socket($expectedHost, $expectedPort);
+
+        $this->assertSame($expectedHost, $socket->host());
+        $this->assertSame($expectedPort, $socket->port());
+    }
+}
